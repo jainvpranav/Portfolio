d27407f91ee962f490761516770d74a7b3745495
Pranav V <100434347+jainvpranav@users.noreply.github.com>
2024-01-18 16:45:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Navbar
+
+{// Navbar
 const navabr2 = document.getElementsByClassName("navbar2");
 const hamburger = document.getElementById("hamburger");
 window.addEventListener("resize", () => {
@@ -15,13 +16,10 @@ hamburger.addEventListener("click", () => {
     }
 
 })
+}
 
 
-
-
-
-
-// Projects
+{// Projects
 let projectname = ['SHIFT', 'ROCKVED', 'PingWave','Akelebecar', 'Todo', 'RSM', 'ISTE', 'Memory Lane', 'Grey Texts'];
 let projectimg = ['./assets/projects/big-sale.gif', './assets/projects/rocket-launch.gif', './assets/projects/vulcan-salute.gif', './assets/projects/jeep.gif', './assets/projects/checklist.gif', './assets/projects/supermarket.gif', './assets/projects/sailing.gif', './assets/projects/camera.gif', './assets/projects/lock.gif'];
 let projectdescription = ['Your AI Designer', 'Wanna Learn Vedic Mathematics? Hop in', 'Your custom secure chatroom🤫', 'Reduce your carbonfootprint', 'To not get your work done on time', 'Manage your retail Store!', 'ISTE Events', 'Refresh your memories', 'Shhh! None knows!'];
@@ -48,9 +46,9 @@ for (var i = 0; i < projectname.length; i++) {
 
     cardcontainer.append(card);
 }
+}
 
-
-//Experience
+{//Experience
 let exprole = ['Lead', 'Aiml Intern', 'Junior Web Developer', 'C Developer'];
 let expcompany = ['Google Developer Student Clubs GMIT', 'DLithe', 'Zummit InfoLabs', 'DeCatalyst'];
 let expduration = ['August 2023 - Present', 'August 2023 - September 2023', 'March 2022 - July 2022', 'February 2022 - April 2022'];
@@ -95,5 +93,5 @@ for(var i=0; i< exprole.length; i++) {
     experiencecard.append(experiencedescription);
 
     experiencemaincontainer.append(experiencecard)
-
+}
 }
